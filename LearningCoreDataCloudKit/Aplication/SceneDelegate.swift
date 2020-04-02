c8e3bc14e2a5f567e894f07eda4e7f6bc0826a87
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-04-02 16:39:27 -0300

    Atualizando UI com os fetchs

--- LearningCoreDataCloudKit/Aplication/SceneDelegate.swift
+++ LearningCoreDataCloudKit/Aplication/SceneDelegate.swift
@@ -18,6 +18,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let navigationController = self.window?.rootViewController as! UINavigationController
+        let mainVC = navigationController.viewControllers[0] as! ViewController
+
+        let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        viewContext.automaticallyMergesChangesFromParent = true
+
+        mainVC.managedObjectContext = viewContext
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
